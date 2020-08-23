87e49744da72a2bf06513a1e8bdb866920aa46fe
Jakub Kosciuszko <jkosciuszko@gmail.com>
2020-08-23 14:09:06 +0200

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,20 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Jakub";
+        System.out.println(myName);
+        long myAge = 922337203685477580l;
+        System.out.println(myAge);
+        double myHeight =  1.73;
+        System.out.println(myHeight);
+        char carClass = 'X';
+        System.out.println(carClass);
+        boolean isGoodTester = true;
+        System.out.println(isGoodTester);
+        String kW = "Witammy na kursie";
+        System.out.print(kW);
+        int kursNum = 33;
+        System.out.println(kursNum);
+        int num1 = 10, num2 = 20;
+        System.out.println(num1 + num2);
+        }
+}
