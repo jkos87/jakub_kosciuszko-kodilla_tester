d57323601dc47202dc600a65b0d2574565351d74
Jakub Kosciuszko <jkosciuszko@gmail.com>
2020-08-26 23:24:56 +0200

    Commit message

--- kodilla-intro/src/main/java/Leapyear.java
+++ kodilla-intro/src/main/java/Leapyear.java
@@ -1,6 +1,6 @@
 public class Leapyear {
     public static void main(String[] args) {
-        int year = 2020;
+        int year = 2000;
         System.out.print(year);
 
         if ((year%4 ==0 && year%100!=0) || (year%400 ==0)){
