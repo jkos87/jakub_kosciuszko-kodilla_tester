360b94abfca5795d49a83039db05f4f00e052ba8
Jakub Kosciuszko <jkosciuszko@gmail.com>
2020-10-05 23:55:04 +0200

    MainExtended - "switch"

--- kodilla-intro/src/main/java/Colors.java
+++ kodilla-intro/src/main/java/Colors.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Colors {
+public class Colors {
+    enum MyColors {
+        B, /* Black */
+        O, /* Oragne */
+        R, /* Red */
+        S, /* Silver */
+    }
+
+    public static void main (String[] args){
+        System.out.println("Select color: G-Grey, B-Black, O-Orange, S-Silver");
+        UserColors color1 = new UserColors();
+        String colorFirstLetter = UserColors.getUserFirstLetter();
+
+    MyColors myColor = MyColors.valueOf(colorFirstLetter);
+
+    switch (myColor) {
+        case B:
+            System.out.println("Black");
+            break;
+        case O:
+            System.out.println("Orange");
+            break;
+        case R:
+            System.out.println("Red");
+            break;
+        case S:
+            System.out.println("Silver");
+            break;
+        }
+    }
 }
+
