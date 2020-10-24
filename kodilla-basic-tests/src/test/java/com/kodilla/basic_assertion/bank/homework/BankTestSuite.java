1ab26463d275514b279699182ea95be95fd9bb65
Jakub Kosciuszko <jkosciuszko@gmail.com>
2020-10-24 11:44:03 +0200

    bankomat testy

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/bank/homework/BankTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/bank/homework/BankTestSuite.java
@@ -8,32 +8,75 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BankTestSuite {
 
+    CashMachine cm1 = new CashMachine(), cm2 = new CashMachine();
+    Bank bank1 = new Bank();
 
     @Test
     public void shouldCountWithdraws() {
-        CashMachine cm1 = new CashMachine(), cm2 = new CashMachine();
-        cm1.add(100);
-        cm1.add(500);
         cm1.add(-300);
         cm1.add(-300);
-
-        cm2.add(500);
         cm2.add(-60);
 
-        Bank bank1 = new Bank();
+        bank1.add(cm1);
+        bank1.add(cm2);
+        assertEquals(3, bank1.bankWithdrawCount()); // ilość wypłat
+    }
+
+    @Test
+    public void shoudlGetAmountWithdraw() {
+        cm1.add(-100);
+        cm2.add(-200);
+
         bank1.add(cm1);
         bank1.add(cm2);
 
-        assertEquals(3,bank1.bankWithdrawCount());
-        assertEquals(660,bank1.withdraw());
-        assertEquals(220, bank1.withdrawAver());
-        assertEquals(440, bank1.balance());
-
-        assertEquals(1100, bank1.deposit());
-        assertEquals(3, bank1.bankDepositCount());
-        assertEquals(366.7, bank1.averDeposit(),0.1);
-
-
-
+        assertEquals(300, bank1.withdraw());
     }
+
+    @Test
+    public void shouldGetWithdrawAverage() {
+        cm1.add(-300);
+        cm2.add(-100);
+
+        bank1.add(cm1);
+        bank1.add(cm2);
+
+        assertEquals(200, bank1.withdrawAver());
+    }
+
+    @Test
+    public void shouldGetAmountDeposit() {
+        cm1.add(300);
+        cm2.add(100);
+
+        bank1.add(cm1);
+        bank1.add(cm2);
+
+        assertEquals(400, bank1.deposit());
+    }
+
+    @Test
+    public void shouldGetDepositAverage() {
+        cm1.add(300);
+        cm2.add(500);
+
+        bank1.add(cm1);
+        bank1.add(cm2);
+
+        assertEquals(400, bank1.averDeposit());
+    }
+
+    @Test
+    public void shouldCountDeposit() {
+        cm1.add(300);
+        cm2.add(500);
+        cm2.add(100);
+
+        bank1.add(cm1);
+        bank1.add(cm2);
+
+        assertEquals(3, bank1.bankDepositCount());
+    }
+
 }
+
